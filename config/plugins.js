82a7156a463ae469a46bb6f877dd9847fdcf2d25
Max Naylor <max_naylor@yahoo.co.uk>
2024-11-03 13:00:19 +0000

    Added sendgrid plugin

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'support@icelandicwithmax.com',
+        defaultReplyTo: 'support@icelandicwithmax.com',
+      },
+    },
+  },
 });
