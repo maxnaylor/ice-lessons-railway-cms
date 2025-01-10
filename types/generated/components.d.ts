ebcc55a8354cc5da1a162807de82989d619033a5
Max Naylor <max_naylor@yahoo.co.uk>
2025-01-10 11:10:58 +0000

    Added rowSpan to paradigm cell

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -244,6 +244,7 @@ export interface ParadigmParadigmCell extends Struct.ComponentSchema {
   attributes: {
     colSpan: Schema.Attribute.Integer;
     content: Schema.Attribute.Blocks;
+    rowSpan: Schema.Attribute.Integer;
     width: Schema.Attribute.Integer;
   };
 }
